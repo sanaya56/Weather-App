e661b3be31ab776636754870027d5c3569c9a4e4
sanya <sanya567kumari@gmail.com>
2023-09-08 18:35:40 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [data, setData]= useState({})
   const [location,setLocation] = useState('')
 
-  const url='https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=302d93de3a5255900459c32de09e601e'
+  const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=302d93de3a5255900459c32de09e601e`
 
   const searchLocation = (event) =>{
     if(event.key==='Enter'){
@@ -32,30 +32,34 @@ function App() {
       <div className='container'>
         <div className='top'>
           <div className='location'>
-            <p>Dallas</p>
+            <p>{data.name}</p>
           </div>
           <div className='temp'>
-            <h1>65 F</h1>
+           {data.main? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
           <div className='description'>
-            <p>Clouds</p>
+            {data.weather? <p>{data.weather[0].main}</p> : null}
           </div>
           
         </div>
-        <div className='bottom'>
-          <div className='feels'>
-            <p className='bold'>65 F</p>
-            <p>Feels Like</p>
-          </div>
-          <div className='humidity'>
-            <p className='bold'>20%</p>
-            <p>Humidity</p>
-          </div>
-          <div className='wind'>
-            <p className='bold'>12 MPH</p>
-            <p>Wind Speed</p>
-          </div>
-        </div>
+        {data.name!=undefined &&
+           <div className='bottom'>
+           <div className='feels'>
+             {data.main ? <p  className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+             <p>Feels Like</p>
+           </div>
+           <div className='humidity'>
+             {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+             <p>Humidity</p>
+           </div>
+           <div className='wind'>
+             {data.wind ? <p className='bold'>{data.wind.speed}MPH</p> : null}
+             <p>Wind Speed</p>
+           </div>
+         </div>
+
+        }
+        
       </div>
      
     </div>
